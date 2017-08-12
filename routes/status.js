b7d71f474c091a12bdf3e5779f6348eccd252262
DJL箫氏 <1281233206@qq.com>
2017-08-12 13:44:31 +0800

    '添加认证访问实时列表路由'

--- routes/status.js
+++ routes/status.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET CPU Loading Status. */
-router.get('/cpu_usage', function(req, res, next) {
+router.get('/misc_stat/cpu_usage', function(req, res, next) {
     res.json({
         "code": 0,
         "msg": {
@@ -15,7 +15,7 @@ router.get('/cpu_usage', function(req, res, next) {
 });
 
 /* GET CPU Temperature */
-router.get('/cpu_temperature', function(req, res, next) {
+router.get('/misc_stat/cpu_temperature', function(req, res, next) {
     res.json({
         code: 0,
         msg: {
@@ -23,6 +23,57 @@ router.get('/cpu_temperature', function(req, res, next) {
             "cpu1": [Math.random()*100, 40, 100],
             "cpu2": [Math.random()*100, 45, 60],
             "cpu3": [Math.random()*100, 33, 60]
+        }
+    });
+});
+
+router.get('/misc_stat/iface_speed', function(req, res, next) {
+    res.json({
+        code: 0,
+        msg: {
+            "cpu0": [Math.random()*300, Math.random()*500],
+            "cpu1": [Math.random()*300, Math.random()*500],
+            "cpu2": [Math.random()*300, Math.random()*500],
+            "cpu3": [Math.random()*300, Math.random()*500]
+        }
+    });
+});
+
+// 认证访问实时列表
+router.get('/access/get_list', function(req, res, next) {
+    res.json({
+        code: 0,
+        msg: {
+           list:[{
+               "time": "2017-06-06 15:35:16",
+               "src": "19.18.102.10",
+               "country": "CN",
+               "username": "user@11.com",
+               "packet_len": 425,
+               "elapsed": 11,
+               "code": 0,
+               "result": "successfully"
+           },{
+               "time": "2017-07-05 15:35:16",
+               "src": "192.168.102.10",
+               "country": "-",
+               "username": "user@11.com",
+               "packet_len": 42500,
+               "elapsed": 11,
+               "code": -123,
+               "result": "the authenication packet is too long"
+           },{
+               "time": "2017-08-05 15:35:16",
+               "src": "192.168.102.10",
+               "country": "-",
+               "username": "user@11.com",
+               "packet_len": 42500,
+               "elapsed": 11,
+               "code": -123,
+               "result": "the authenication packet is too long"
+           }],
+            "total": 2,
+            "cur": 1
         }
     });
 });
